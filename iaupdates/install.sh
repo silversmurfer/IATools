94c5b0025cd03e84d03fb109cb5bc4f790ec84f6
silversmurfer <nik.shaw@gmail.com>
2019-12-09 11:26:42 +0000

    updated comments

--- iaupdates/install.sh
+++ iaupdates/install.sh
@@ -8,7 +8,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 #-----------------------------------------------------------
 
-#Step X) Clean UP----------------------------------
+#Step X) Clean UP-------------------------------------------
 #-----------------------------------------------------------
 cd /tmp
 rm -rf /tmp/IATools
@@ -21,42 +21,42 @@ git clone https://github.com/silversmurfer/IATools.git
 #-----------------------------------------------------------
 
 
-#Step 3) Update motd----------------------------------
+#Step 3) Update motd----------------------------------------
 #-----------------------------------------------------------
 cd /tmp
 echo "Updating banner"
 cp -f /tmp/IATools/iaupdates/motd /etc
 #-----------------------------------------------------------
 
-#Step 4) Fix Bluray theme----------------------------------
+#Step 4) Fix Bluray theme-----------------------------------
 #-----------------------------------------------------------
 cd /tmp
 echo "Fixing Bluray theme to be family friendly"
 cp -f /tmp/IATools/iaupdates/themes/bluray/auto-allgames/_inc/system.png /etc/emulationstation/themes/bluray/auto-allgames/_inc/
 #-----------------------------------------------------------
 
-#Step 5) Fix folder permissions on config files----------------------------------
+#Step 5) Fix folder permissions on config files-------------
 #-----------------------------------------------------------
 cd /tmp
 echo "Fixing folder permissions on configs"
 chown -R pi:pi /opt/retropie/configs/*
 #-----------------------------------------------------------
 
-#Step 6) Fix retroarch config files----------------------------------
+#Step 6) Fix retroarch config files-------------------------
 #-----------------------------------------------------------
 cd /tmp
 echo "Updating retroarch config files"
 cp -f /tmp/IATools/iaupdates/configs/all/retronetplay.cfg /opt/retropie/configs/all/
 #-----------------------------------------------------------
 
-#Step 7) Fix emulationstation collections----------------------------------
+#Step 7) Fix emulationstation collections-------------------
 #-----------------------------------------------------------
 cd /tmp
 echo "Updating emulationstaion collections"
 cp -rf /tmp/IATools/iaupdates/configs/all/emulationstation/collections /opt/retropie/configs/all/emulationstation/
 #-----------------------------------------------------------
 
-#Step X) Clean UP----------------------------------
+#Step X) Clean UP-------------------------------------------
 #-----------------------------------------------------------
 cd /tmp
 echo "Cleaning up after"
